698c750d260e82b55b78b0574c3301163a2810de
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2022-02-25 09:37:08 +0100

    Editted a php file to trigger workflow

--- app/Http/Controllers/Skills/CreateController.php
+++ app/Http/Controllers/Skills/CreateController.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Skills\CreateRequest;
 use App\Http\Resources\SkillResource;
 use App\Models\Skill;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class CreateController extends Controller
 {
-    public function __invoke(CreateRequest $request)
+    public function __invoke(CreateRequest $request): JsonResponse
     {
         return $this->render(
             data: new SkillResource(Skill::query()->create($request->validated())),
